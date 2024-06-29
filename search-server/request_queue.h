0bc4b6fee45929034c84dba7dbfd06d5d66fbbb7
Mikhail Shutov <shutovmik8@gmail.com>
2024-06-29 13:20:11 +0300

    Iteration 1

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -1,32 +1,16 @@
 #pragma once
+#include "search_server.h"
+#include "document.h"
 #include <deque>
 #include <string>
 #include <vector>
-#include "search_server.h"
-#include "document.h"
 
 class RequestQueue {
 public:
     explicit RequestQueue(const SearchServer&);
     
     template <typename DocumentPredicate>
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-        std::vector<Document> ret = server.FindTopDocuments(raw_query, document_predicate);
-        current_time += 1;
-        bool empty = ret.empty();
-        if (empty) {
-            ++quant_no_result;
-        }
-        requests_.push_back({empty, current_time});
-        if (current_time > min_in_day_) {
-            if (requests_.front().no_result) {
-                --quant_no_result;
-            }
-            requests_.pop_front();
-        }
-        return ret;
-    }
-    
+    std::vector<Document> AddFindRequest(const std::string&, DocumentPredicate);  
     std::vector<Document> AddFindRequest(const std::string&, DocumentStatus); 
     std::vector<Document> AddFindRequest(const std::string&);
     int GetNoResultRequests() const;
@@ -42,3 +26,21 @@ private:
     int current_time = 0;
     int quant_no_result = 0;
 };
+
+template <typename DocumentPredicate>
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+    std::vector<Document> ret = server.FindTopDocuments(raw_query, document_predicate);
+    current_time += 1;
+    bool empty = ret.empty();
+    if (empty) {
+        ++quant_no_result;
+    }
+    requests_.push_back({empty, current_time});
+    if (current_time > min_in_day_) {
+        if (requests_.front().no_result) {
+            --quant_no_result;
+        }
+        requests_.pop_front();
+    }
+    return ret;
+}
